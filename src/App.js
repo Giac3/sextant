7504fa1374bfc7dd5e72652a6ef45c85924fe579
Giacomo Pessotto <giacomopesso@gmail.com>
2022-10-31 11:25:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wow new app fro cisco
         </a>
       </header>
     </div>
